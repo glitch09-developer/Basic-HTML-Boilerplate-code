a37965be81eb6117a84bb906016c259851e8c57a
glitch09-developer <royal.glitch.09@gmail.com>
2025-10-05 19:00:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert('This code was executed using JavaScript');
+//   Dangerous part starts from  here please dont run under this or it may hang your pc 
+let i = '5';
+if (i>4) {
+  console.log("DONT REPEAT") } else {
+  console.log('Infinite loop') 
+};
