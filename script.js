54f64a8c16e9929d2e1fa7bcc250e6cfa6c21da3
glitch09-developer <royal.glitch.09@gmail.com>
2025-10-05 19:03:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 alert('This code was executed using JavaScript');
 //   Dangerous part starts from  here please dont run under this or it may hang your pc 
 let i = '5';
-if (i>4) {
-  console.log("DONT REPEAT") } else {
-  console.log('Infinite loop') 
-};
+if (i > 4) {
+  // The 'if' condition (5 > 4) is met, so this block executes.
+  while (true) {
+    // This is an infinite loop that will run forever.
+    console.log("DONT REPEAT, but I will!"); 
+  }
+} else {
+  // This block will never be reached in this code.
+  console.log('Infinite loop');
+}
