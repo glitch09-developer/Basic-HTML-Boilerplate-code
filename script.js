04420874be5a8e62323f7fc95ef43ffddc500a3e
glitch09-developer <royal.glitch.09@gmail.com>
2025-10-10 16:25:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 alert('This code was executed using JavaScript');
-//   Dangerous part starts from  here please dont run under this or it may hang your pc 
-let i = 5;
-let a = 2;
-do {
-  console.log(`infinite loop ${a} and ${i}`)
-  i++
-  a++
-} while (i > a);
